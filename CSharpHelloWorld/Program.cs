431fd3c1bed7758cc2b259a7f3c146db7a752523
littletrainee <arechery.a1111@gmail.com>
2023-11-18 20:28:59 +0800

    Create First Setting

---
+++ CSharpHelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpHelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
